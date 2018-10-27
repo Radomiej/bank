f69e2803484bc50f362222e415e7529b372aa5c2
Radomiej <radek@o2.pl>
2018-10-27 10:57:22 +0200

    Add Swagger with UI to project.

--- src/main/java/pl/studia/bank/controller/BankController.java
+++ src/main/java/pl/studia/bank/controller/BankController.java
@@ -1,7 +1,19 @@
 package pl.studia.bank.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.studia.bank.model.OperationResult;
 
 @Controller
+@RequestMapping("api/v1/controller")
 public class BankController {
+
+    @PostMapping("/addBankAccount")
+    public ResponseEntity<OperationResult> AddBankAccount(){
+
+        return ResponseEntity.notFound().build();
+    }
 }
---
+++ src/main/java/pl/studia/bank/model/OperationResult.java
@@ -0,0 +1,4 @@
+package pl.studia.bank.model;
+
+public class OperationResult {
+}
